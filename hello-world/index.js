9ba5d62c0ecadfe451fcaa8cbc0315524b7c6ad0
Nabeen-Thapa <nabeen2058@gmail.com>
2024-10-07 18:11:24 +0545

    add app that accress through other link /hello

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,7 +1,14 @@
 var express= require('express');
 var app = express();
-
+//at how page
 app.get('/', function(req,res){
-    res.send("hello world");
+    res.send("hello world, this is home page");
 });
+
+//accress through other link
+
+app.get('/hello', function(req,res){
+    res.send("hello world, you are accressing this form link");
+});
+
 app.listen(3000);
